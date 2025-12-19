a17d130d530f32fefbaf7dee6ec7a9e49dcaf40c
Enzo1987Pavel <Enzo1987@mail.ru>
2025-12-19 22:10:41 +0300

    Add static files and template

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from os import environ
 from flask import Flask, render_template
+
+flask_port = environ.get("Flask_port")
 
 app = Flask(__name__)
 
@@ -9,4 +12,4 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=flask_port, debug=True)
