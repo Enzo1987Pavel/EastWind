10b23fbb0ce931fd0769ea66f801a06e0d71a35f
Enzo1987Pavel <enzo1987@mail.ru>
2025-12-25 12:47:30 +0300

    change host and port

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from os import environ
 from flask import Flask, render_template
 
-flask_port = environ.get("Flask_port")
-
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def base():  # put application's code here
     return render_template("base.html")
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=flask_port, debug=True)
+    host = environ.get("FLASK_RUN_HOST")
+    flask_port = int(environ.get("FLASK_RUN_PORT"))
+    app.run(host=host, port=flask_port, debug=True)
