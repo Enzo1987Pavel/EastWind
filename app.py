ed1548cc4a2cfe89fde86d6ba05b72431831f26e
Enzo1987Pavel <Enzo1987@mail.ru>
2026-01-06 15:45:00 +0300

    Add HTML styles for main page

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():  # put application's code here
-    return render_template("main.html", name="main_page", title="Главная")
+    return render_template("fatbike.html", name="main_page", title="Главная")
 
-# @app.route('/')
-# def base():  # put application's code here
-#     return render_template("base.html")
+@app.route('/fatbike')
+def fatbike_page():  # put application's code here
+    return render_template("fatbike.html", name="fatbike_page", title="Fat-bike")
 
 
 if __name__ == '__main__':
