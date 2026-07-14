80fa0a90cc6dcce9ef53100bb19c730c99b75b3b
Enzo1987Pavel <enzo1987@mail.ru>
2026-07-14 21:22:10 +0300

    Factorial file for students

---
+++ factorial.py
@@ -0,0 +1,14 @@
+input_string = input("Введите целое число: ")
+
+
+def factorial_interact(n: int):
+    if n < 0:
+        raise ValueError("Факториал не определён для отрицательных чисел")
+    result = 1
+    for i in range(2, n + 1):  # начинаем с 2, так как умножение на 1 не меняет результат
+        result *= i
+    print(str(result))
+    return result
+
+
+factorial_interact(int(input_string))
